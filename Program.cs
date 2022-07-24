075ae8c3ae0dd3e59f1402ee565fde26e46273ea
Yogesh <yogeshbietjhs@gmail.com>
2022-07-25 01:19:16 +0530

    all read, write, delete etc

--- Program.cs
+++ Program.cs
@@ -1,10 +1,60 @@
-﻿namespace FileIO
+﻿using System;
+using System.IO;
+namespace FileIO
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Program.ReadFile();
+
+            Program.Deletefile();
+
+
+            //StreamReaderWriterOops readThoughStreamReader = new StreamReaderWriterOops();
+            //readThoughStreamReader.ReadFrromReader();
+            //readThoughStreamReader.WriteUsingStreamWriter();
+
+        }
+        public static void FileExists()
+        {
+            string path = @"/Users/yogesh/Desktop/bridgelab/bridgelab/FileIO/FileIO/Example.txt";
+            if (File.Exists(path))
+            {
+                Console.WriteLine("file exist");
+            }
+            
+            Console.ReadKey();
+        }
+        public static void ReadFile()
+        {
+            string path = @"/Users/yogesh/Desktop/bridgelab/bridgelab/FileIO/FileIO/Example.txt";
+            string[] lines;
+            lines = File.ReadAllLines(path);
+            Console.WriteLine(lines[0]);
+
+            Console.ReadKey();
+
+        }
+
+        public static void Filecopy()
+        {
+            string path = @"/Users/yogesh/Desktop/bridgelab/bridgelab/FileIO/FileIO/Example.txt";
+            string copypath = @"/Users/yogesh/Desktop/bridgelab/bridgelab/FileIO/FileIO/copyExample.txt";
+            File.Copy(path, copypath, true);
+            Console.ReadKey();
+
+        }
+        public static void Deletefile()
+        {
+            string copypath = @"/Users/yogesh/Desktop/bridgelab/bridgelab/FileIO/FileIO/TextFile1.txt";
+
+            File.Delete(copypath);
+            Console.ReadKey();
+
+
+
         }
     }
+    
 }
---
+++ StreamReaderWriterOops.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIO
+{
+    public class StreamReaderWriterOops
+    {
+        public void WriteUsingStreamWriter()
+        {
+            string path = @"/Users/yogesh/Desktop/bridgelab/bridgelab/FileIO/FileIO/Example.txt";
+            using (StreamWriter sr = File.AppendText(path))
+            {
+                sr.WriteLine("heLLO World");
+                sr.Close();
+                Console.WriteLine(File.ReadAllText(path));
+            }
+            Console.ReadKey();
+
+        }
+
+        public void ReadFrromReader()
+        {
+            string path = @"/Users/yogesh/Desktop/bridgelab/bridgelab/FileIO/FileIO/Example.txt";
+            using (StreamReader sr = File.OpenText(path))
+            {
+                String s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+                Console.ReadLine();
+            }
+
+        }
+    }
+    
+}
